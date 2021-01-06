5aaf09d64b0a9ae5d260f3aaaff6f6f05ef6a580
Brendan O'Donoghue <bodonoghue85@gmail.com>
2021-01-06 21:48:39 +0000

    add sdplib

---
+++ run_sdplib_problems.py
@@ -0,0 +1,67 @@
+'''
+Run sdplib problems for the OSQP paper
+
+This code tests the solvers:
+    - OSQP
+    - GUROBI
+    - MOSEK
+
+'''
+from sdplib_problems.sdplib_problem import SDPLIBRunner
+import solvers.solvers as s
+from utils.benchmark import compute_stats_info
+import os
+import argparse
+import shutil
+
+
+parser = argparse.ArgumentParser(description='SDPLIB Runner')
+parser.add_argument('--high_accuracy', help='Test with high accuracy', default=False,
+                    action='store_true')
+parser.add_argument('--verbose', help='Verbose solvers', default=True,
+                    action='store_true')
+parser.add_argument('--parallel', help='Parallel solution', default=False,
+                    action='store_true')
+
+args = parser.parse_args()
+high_accuracy = args.high_accuracy
+verbose = args.verbose
+parallel = args.parallel
+
+print('high_accuracy', high_accuracy)
+print('verbose', verbose)
+print('parallel', parallel)
+
+solvers=[s.SCS, s.OSQP, s.qpOASES, s.ECOS]
+
+# Shut up solvers
+if verbose:
+    for key in s.settings:
+        s.settings[key]['verbose'] = True
+
+OUTPUT_FOLDER = 'sdplib_problems'
+
+# Run all examples
+sdplib_runner = SDPLIBRunner(solvers,
+                             s.settings,
+                             OUTPUT_FOLDER)
+
+# debug
+sdplib_runner.problems = ["gpp100"]
+#sdplib_runner.problems = \
+#  sdplib_runner.problems[sdplib_runner.problems.index("pilot4i"):]
+#
+#probs = sdplib_runner.problems
+#for prob in probs:
+#  sdplib_runner = sdplibRunner(solvers,
+#                             s.settings,
+#                             OUTPUT_FOLDER)
+#  sdplib_runner.problems = [prob]
+#  sdplib_runner.solve(parallel=parallel, cores=12)
+#  shutil.rmtree("./results/sdplib_feasible")
+
+
+sdplib_runner.solve(parallel=parallel, cores=12)
+# Compute results statistics
+compute_stats_info(solvers, OUTPUT_FOLDER,
+                   high_accuracy=high_accuracy)
