062fed46ea3dcc12832d326e1ae2c6f8be0cd494
Brendan O'Donoghue <bodonoghue85@gmail.com>
2021-02-03 11:02:21 +0000

    getting close to final results

--- utils/make_table.py
+++ utils/make_table.py
@@ -23,19 +23,21 @@ def get_data(solvers, output_folder, infeasible_test):
 
 
 def make_latex_table(solvers, output_folder, infeasible_test):
-    data = get_data(solvers, output_folder, infeasible_test)
+    orig_data = get_data(solvers, output_folder, infeasible_test)
     # want this type of output:
     #         solver 1 | solver 2 | solver 3
     # prob 1    time   |  FAIL    |  time
     # prob 2
+    data = {}
     for solver in solvers:
-      df = data[solver]
+      df = orig_data[solver].copy()
       if infeasible_test:
         df.loc[~df['status'].str.contains('infeasible')] = np.nan
       else:
         df.loc[df['status'] != 'optimal'] = np.nan
       df = df.rename(columns={'run_time': solver.strip()})
       data[solver] = df[solver]
+
 
     probs = output_folder.replace('_', ' ')
     column_format = 'l' + 'c' * len(data)
@@ -60,13 +62,34 @@ def make_latex_table(solvers, output_folder, infeasible_test):
     with open(table_path, "w") as f:
       f.write(table)
 
-    path = os.path.join('.', 'results', output_folder, 'geom_mean.csv')
+    path = os.path.join('.', 'results', output_folder, 'geom_mean_run_time_normalized.csv')
     df = pd.read_csv(path)
     table = df.to_latex(float_format="%.2f", index=False,
                         caption=f'Solver geometric means on {probs} problems.',
                         label=f't-{probs}-geom_mean',
                         column_format=column_format)
     table_path = os.path.join('.', 'results', output_folder, f'{output_folder}_geom_mean_table.tex')
+    with open(table_path, "w") as f:
+      f.write(table)
+
+    # same again, except iter instead of run_time 
+    data = {}
+    for solver in solvers:
+      df = orig_data[solver].copy()
+      if infeasible_test:
+        df.loc[~df['status'].str.contains('infeasible')] = np.nan
+      else:
+        df.loc[df['status'] != 'optimal'] = np.nan
+      df = df.rename(columns={'iter': solver.strip()})
+      data[solver] = df[solver]
+
+    #df = pd.concat([d for d in data.values()])
+    df = pd.DataFrame(data)
+    table = df.to_latex(float_format="%i", longtable=True, index_names=False,
+                        caption=f'Solver iters on {probs} problems.',
+                        label=f't-{probs}-iters',
+                        column_format=column_format)
+    table_path = os.path.join('.', 'results', output_folder, f'{output_folder}_iters.tex')
     with open(table_path, "w") as f:
       f.write(table)
 
