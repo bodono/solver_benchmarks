7fe2c58828a65c73557502e505e6dd51238c67f4
Brendan O'Donoghue <bodonoghue85@gmail.com>
2021-02-08 17:10:07 +0000

    dl all qplib probs

--- run_qplib_problems.py
+++ run_qplib_problems.py
@@ -32,7 +32,7 @@ print('verbose', verbose)
 print('parallel', parallel)
 
 OUTPUT_FOLDER = 'qplib_problems'
-solvers=[s.SCS, s.OSQP, s.COSMO, s.SCS_ALT]#, s.SCS_AA, s.ECOS, s.qpOASES]
+solvers=[s.SCS, s.OSQP, s.COSMO]#, s.SCS_AA, s.ECOS, s.qpOASES]
 
 # Shut up solvers
 if verbose:
