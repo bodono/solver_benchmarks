02d94508cdf0678616e7e9ad9ce31a01ddb07fe2
Brendan O'Donoghue <bodonoghue85@gmail.com>
2021-02-17 20:33:47 +0000

    fix edge cases of mps reading

--- problem_classes/qpsreader.py
+++ problem_classes/qpsreader.py
@@ -1,22 +1,29 @@
 import numpy as np
 from scipy import sparse
-from cylp.py.QP import QPSReader 
+from cylp.py.QP import QPSReader
 
 
 def readMpsLp(filename):
-    (P, c, A, b, G, c_low, c_up, x_low, x_up, _, _, _, _) = QPSReader.readQPS(filename)
+    (P, c, A, b, G, c_low, c_up, x_low, x_up, n, _, _, _) = QPSReader.readQPS(filename)
     assert not P
-    (m, n) = A.shape
-    l = b.copy()
-    u = b.copy()
+
+    mat = sparse.coo_matrix((0, n))
+    l = np.array([])
+    u = np.array([])
+
+    if isinstance(b, np.ndarray):
+        l = b.copy()
+        u = b.copy()
+        mat = sparse.vstack((mat, A))
+
     if isinstance(c_low, np.ndarray):
         l = np.hstack((l, c_low))
         u = np.hstack((u, c_up))
-        A = sparse.vstack((A, G))
+        mat = sparse.vstack((mat, G))
 
     if isinstance(x_low, np.ndarray):
         l = np.hstack((l, x_low))
         u = np.hstack((u, x_up))
-        A = sparse.vstack((A, sparse.eye(n, format='dok')))
-    return (A, c, l, u)
+        mat = sparse.vstack((mat, sparse.eye(n, format='dok')))
+    return (mat, c, l, u)
 
