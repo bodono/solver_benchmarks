1bdf1e0dd31ab8f0e51bd7112fe85bf2c4a1a970
Brendan O'Donoghue <bodonoghue85@gmail.com>
2021-09-09 15:39:05 +0100

    update parametric problems

--- parametric_problems/lasso.py
+++ parametric_problems/lasso.py
@@ -7,13 +7,13 @@ import os
 from solvers.solvers import SOLVER_MAP  # AVOID CIRCULAR DEPENDENCY
 from problem_classes.lasso import LassoExample
 from utils.general import make_sure_path_exists
-# import osqppurepy as osqp
-import osqp
+import scipy.sparse
+import scs
 
 
 class LassoParametric(object):
     def __init__(self,
-                 osqp_settings,
+                 settings,
                  dimension,
                  minimum_lambda_over_max=0.01,
                  n_problems=100):
@@ -21,12 +21,12 @@ class LassoParametric(object):
         Generate Parametric Lasso object
 
         Args:
-            osqp_settings: osqp solver settings
+            settings: solver settings
             dimension: leading dimension for the problem
             minimum_lambda_over_max: min ratio between lambda and lambda_max
             n_problem: number of lasso problems to solve
         """
-        self.osqp_settings = osqp_settings
+        self.settings = settings
         self.dimension = dimension
         self.minimum_lambda_over_max = minimum_lambda_over_max
         self.n_problems = n_problems
@@ -54,7 +54,7 @@ class LassoParametric(object):
         #  print("Solving without warm start")
         # Solution directory
         no_ws_path = os.path.join('.', 'results', 'parametric_problems',
-                                  'OSQP no warmstart',
+                                  'no warmstart',
                                   'Lasso',
                                   )
 
@@ -71,21 +71,38 @@ class LassoParametric(object):
                 # Update lambda
                 instance.update_lambda(lambda_val)
 
-                # Solve problem
-                m = osqp.OSQP()
-                m.setup(qp['P'], qp['q'], qp['A'], qp['l'], qp['u'],
-                        **self.osqp_settings)
-                r = m.solve()
+                A = qp['A']
+                (m, n) = A.shape
+                # Hack out the equality constraints
+                idxs = (qp['u'] - qp['l'] < 1e-6)
+                idxs &= (qp['u'] < 1e20)
+                idxs &= (qp['l'] > -1e20)
+                o_idxs = np.array(range(A.shape[0])) # no need +1 for box cone
+                o_idxs = np.hstack((o_idxs[idxs], o_idxs[~idxs]))
+                inv_perm = np.argsort(o_idxs)
+
+                A_scs = scipy.sparse.vstack((A[idxs, :], np.zeros((1, n)), -A[~idxs, :]))
+                b_scs = np.hstack((qp['u'][idxs], 1, np.zeros(m - np.sum(idxs))))
+
+                data = dict(P=scipy.sparse.csc_matrix(qp['P']), c=qp['q'],
+                            A=scipy.sparse.csc_matrix(A_scs), b=b_scs)
+                cone = dict(z=np.int(np.sum(idxs)), bl=qp['l'][~idxs].tolist(),
+                              bu=qp['u'][~idxs].tolist())
+
+                results = scs.solve(data, cone, **self.settings)
+
 
                 # DEBUG
                 #  print("Lambda = %.4e,\t niter = %d" % (lambda_val, r.info.iter))
 
-                if r.info.status != "solved":
-                    print("OSQP no warmstart did not solve the problem")
+                if results['info']['status'] != "solved":
+                    print("SCS no warmstart did not solve the problem")
 
-                solution_dict = {'status': [r.info.status],
-                                 'run_time': [r.info.run_time],
-                                 'iter': [r.info.iter]}
+                run_time = (results['info']['setup_time'] +
+                            results['info']['solve_time']) /1000.
+                solution_dict = {'status': [results['info']['status']],
+                                 'run_time': [run_time],
+                                 'iter': [results['info']['iter']]}
 
                 res_list_no_ws.append(pd.DataFrame(solution_dict))
 
@@ -95,62 +112,3 @@ class LassoParametric(object):
             # Store file
             res_no_ws.to_csv(n_file_name, index=False)
 
-        '''
-        Solve problem with warm start
-        '''
-
-        #  print("Solving with warm start")
-        # Solution directory
-        ws_path = os.path.join('.', 'results', 'parametric_problems',
-                               'OSQP warmstart',
-                               'Lasso',
-                               )
-
-        # Create directory for the results
-        make_sure_path_exists(ws_path)
-
-        # Check if solution already exists
-        n_file_name = os.path.join(ws_path, 'n%i.csv' % self.dimension)
-
-        # Reset problem to first instance
-        instance.update_lambda(lambda_array[0])
-
-        # Setup solver
-        qp = instance.qp_problem
-        m = osqp.OSQP()
-        m.setup(qp['P'], qp['q'], qp['A'], qp['l'], qp['u'],
-                **self.osqp_settings)
-
-        if not os.path.isfile(n_file_name):
-
-            res_list_ws = []  # Initialize results
-            for lambda_val in lambda_array:
-
-                # Update lambda
-                instance.update_lambda(lambda_val)
-                m.update(q=qp['q'])
-
-                # Solve problem
-                r = m.solve()
-
-                # DEBUG
-                #  print("Lambda = %.4e,\t niter = %d" % (lambda_val, r.info.iter))
-
-                if r.info.status != "solved":
-                    print("OSQP warmstart did not solve the problem")
-
-                # Get results
-                solution_dict = {'status': [r.info.status],
-                                 'run_time': [r.info.run_time],
-                                 'iter': [r.info.iter]}
-
-                res_list_ws.append(pd.DataFrame(solution_dict))
-
-            # Get full warm-start
-            res_ws = pd.concat(res_list_ws)
-
-            # Store file
-            res_ws.to_csv(n_file_name, index=False)
-
-        else:
-            res_ws = pd.read_csv(n_file_name)
--- parametric_problems/mpc.py
+++ parametric_problems/mpc.py
@@ -6,25 +6,25 @@ import pandas as pd
 import os
 from problem_classes.control import ControlExample
 from utils.general import make_sure_path_exists
-# import osqppurepy as osqp
-import osqp
+import scipy.sparse
+import scs
 
 
 class MPCParametric(object):
     def __init__(self,
-                 osqp_settings,
+                 settings,
                  dimension,
                  n_simulation=100):
         """
         Generate MPC problem as parametric QP
 
         Args:
-            osqp_settings: osqp solver settings
+            settings: solver settings
             dimension: leading dimension for the problem
             minimum_lambda_over_max: min ratio between lambda and lambda_max
             n_simulation: number of MPC problems to solve
         """
-        self.osqp_settings = osqp_settings
+        self.settings = settings
         self.dimension = dimension
         self.n_simulation = n_simulation
 
@@ -45,7 +45,7 @@ class MPCParametric(object):
         '''
         # Solution directory
         no_ws_path = os.path.join('.', 'results', 'parametric_problems',
-                                  'OSQP no warmstart',
+                                  'no warmstart',
                                   'MPC',
                                   )
 
@@ -64,23 +64,40 @@ class MPCParametric(object):
             res_list_no_ws = []  # Initialize results
             for i in range(self.n_simulation):
 
-                # Solve problem
-                m = osqp.OSQP()
-                m.setup(qp['P'], qp['q'], qp['A'], qp['l'], qp['u'],
-                        **self.osqp_settings)
-                r = m.solve()
+                A = qp['A']
+                (m, n) = A.shape
+                # Hack out the equality constraints
+                idxs = (qp['u'] - qp['l'] < 1e-6)
+                idxs &= (qp['u'] < 1e20)
+                idxs &= (qp['l'] > -1e20)
+                o_idxs = np.array(range(A.shape[0])) # no need +1 for box cone
+                o_idxs = np.hstack((o_idxs[idxs], o_idxs[~idxs]))
+                inv_perm = np.argsort(o_idxs)
 
-                solution_dict = {'status': [r.info.status],
-                                 'run_time': [r.info.run_time],
-                                 'iter': [r.info.iter]}
+                A_scs = scipy.sparse.vstack((A[idxs, :], np.zeros((1, n)), -A[~idxs, :]))
+                b_scs = np.hstack((qp['u'][idxs], 1, np.zeros(m - np.sum(idxs))))
 
-                if r.info.status != "solved":
-                    print("OSQP no warmstart did not solve the problem")
+                data = dict(P=scipy.sparse.csc_matrix(qp['P']), c=qp['q'],
+                            A=scipy.sparse.csc_matrix(A_scs), b=b_scs)
+                cone = dict(z=np.int(np.sum(idxs)), bl=qp['l'][~idxs].tolist(),
+                              bu=qp['u'][~idxs].tolist())
+
+                results = scs.solve(data, cone, **self.settings)
+
+                run_time = (results['info']['setup_time'] +
+                            results['info']['solve_time']) /1000.
+                solution_dict = {'status': [results['info']['status']],
+                                 'run_time': [run_time],
+                                 'iter': [results['info']['iter']]}
+
+                if results['info']['status'] != "solved":
+                    print("SCS no warmstart did not solve the problem")
+
 
                 res_list_no_ws.append(pd.DataFrame(solution_dict))
 
                 # Get input
-                U_no_ws[:, i] = r.x[instance.nx * (instance.T + 1):
+                U_no_ws[:, i] = results['x'][instance.nx * (instance.T + 1):
                                     instance.nx * (instance.T + 1)+instance.nu]
 
                 # Propagate state
@@ -103,80 +120,3 @@ class MPCParametric(object):
             # plt.title("No Warm Start")
             # plt.show(block=False)
 
-        '''
-        Solve problem with warm start
-        '''
-        # Solution directory
-        ws_path = os.path.join('.', 'results', 'parametric_problems',
-                               'OSQP warmstart',
-                               'MPC',
-                               )
-
-        # Create directory for the results
-        make_sure_path_exists(ws_path)
-
-        # Check if solution already exists
-        n_file_name = os.path.join(ws_path, 'n%i.csv' % self.dimension)
-
-        # Set initial state as x0
-        instance.update_x0(x0)
-        
-        if not os.path.isfile(n_file_name):
-            # Setup solver
-            m = osqp.OSQP()
-            m.setup(qp['P'], qp['q'], qp['A'], qp['l'], qp['u'],
-                    **self.osqp_settings)
-
-            # Initialize states and inputs for the whole simulation
-            X_ws = np.zeros((instance.nx, self.n_simulation + 1))
-            U_ws = np.zeros((instance.nu, self.n_simulation))
-            X_ws[:, 0] = x0
-
-            res_list_ws = []  # Initialize results
-            for i in range(self.n_simulation):
-
-                # Solve problem
-                r = m.solve()
-
-                if r.info.status != "solved":
-                    print("OSQP no warmstart did not solve the problem")
-
-                # Get results
-                solution_dict = {'status': [r.info.status],
-                                 'run_time': [r.info.run_time],
-                                 'iter': [r.info.iter]}
-
-                res_list_ws.append(pd.DataFrame(solution_dict))
-
-                # Get input
-                U_ws[:, i] = r.x[instance.nx * (instance.T + 1):
-                                 instance.nx * (instance.T + 1)+instance.nu]
-
-                # Propagate state
-                X_ws[:, i + 1] = instance.A.dot(X_ws[:, i]) + \
-                    instance.B.dot(U_ws[:, i])
-
-                # Update initial state
-                instance.update_x0(X_ws[:, i + 1])
-
-                # Update solver
-                m.update(l=instance.qp_problem['l'],
-                         u=instance.qp_problem['u'])
-
-            # Get full warm-start
-            res_ws = pd.concat(res_list_ws)
-
-            # Store file
-            res_ws.to_csv(n_file_name, index=False)
-
-            # # Plot results
-            # import matplotlib.pylab as plt
-            # plt.figure(3)
-            # plt.plot(X_ws.T)
-            # plt.title("X Warm Start")
-            # plt.show(block=False)
-            #
-            # plt.figure(4)
-            # plt.plot(U_ws.T)
-            # plt.title("U Warm Start")
-            # plt.show()
